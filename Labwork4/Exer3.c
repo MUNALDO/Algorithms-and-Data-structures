d86068f39e6298c4f424ba2c1cfaf62c423dcc2d
MunNX <thaiboomnguyen@gmail.com>
2021-10-12 23:54:33 +0700

    Final Vers

--- Labwork4/Exer3.c
+++ Labwork4/Exer3.c
@@ -1,59 +1,17 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include<stdio.h>
+int main(){
+	int sum=0,arr[1000],i,a;
+	char str[1000];
+	
+	scanf("%s", &str);
+	a = strlen(str);
 
-
-struct Digits
-{
-    int data;
-    struct Digits* next;
-};
-
-struct Digits* createnode(int number)
-{
-    struct Digits* temp = (struct Digits*)malloc(sizeof(struct Digits));
-    temp->data=number;
-    temp->next=NULL;
-    return temp;
-}
-
-void display(struct Digits* head)
-{
-    if(head==NULL)
-    {
-        printf("\nList is Empty\n");
-        return;
-    }
-
-    struct Digits *p=head;
-    while(p!=NULL)
-    {
-        printf("\n%d ",p->data);
-        p=p->next;
-    }
-}
-
-void SumOfDigits(struct Digits **head,int number)
-{
-    struct Digits *temp;
-
-    if(*head==NULL)
-    {
-        *head=temp;
-        return;
-    }
-    
-    char sNum[100];
-    itoa(number,sNum,100);
-    printf("%s\n",sNum);
-    
-    
-}
-
-int main()
-{
-    struct Digits *head=NULL;
-
-    SumOfDigits(&head,123);
-    display(head);
+	for(i=0;i<a;i++){
+		arr[i] = str[i] - 48; //ASCII
+		sum += arr[i];
+		printf("%d ", arr[i]);
+	}
+	
+	printf("%d", sum);
+	return 0;
 }
